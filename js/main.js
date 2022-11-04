0767ef14e988492eeb26ab3ec64b7dd49beaf155
mariaximenahpg <federico.businessrico@gmail.com>
2022-11-04 18:27:27 +0100

    mancano i commenti

--- js/main.js
+++ js/main.js
@@ -7,3 +7,44 @@
 // * Pensate prima in italiano.
 // * Dividete in piccoli problemi la consegna.
 // * Individuate gli elementi di cui avete bisogno per realizzare il programma
+
+const fiveRandom = []
+    
+while (fiveRandom.length < 5) {
+    let i
+    i = Math.floor(Math.random() * 100) + 1
+    if (!fiveRandom.includes(i)) {
+        fiveRandom.push(i)
+    }
+}
+alert(fiveRandom)
+console.log(fiveRandom)
+
+
+setTimeout(function(){
+    game()
+}, 30000)
+
+const chances = []
+const correctChoice = []
+
+function game() {
+    let chance
+    while (isNaN(chance) || chances.length < 5) {
+        chance = Number(prompt("Inserisci uno dei numeri che hai memorizzato"))
+        chances.push(chance)
+        if (fiveRandom.includes(chance)){
+            if(correctChoice.includes(chance)){
+                alert("Il numero è già presente. Non sprecare invano i tentativi a tua disposizione :(");
+            } else{
+                correctChoice.push(chance)
+            }
+        }
+    }
+
+console.log(chances)
+console.log(correctChoice)
+    if (chances.length === 5) {
+        alert(`Hai indovinato ${correctChoice.length} numeri: ${correctChoice}`)
+    }
+}
